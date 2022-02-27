02b3d1892779586f79762638e1375fda37d1eb0d
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 01:39:22 +0200

    Added bad request exception

---
+++ src/Application/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ITranslateTrainer.Application.Common.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string? msg) : base(msg)
+        {
+        }
+
+        public BadRequestException(string? msg, Exception? inner) : base(msg, inner)
+        {
+        }
+    }
+}
