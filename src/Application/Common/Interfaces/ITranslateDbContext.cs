ee3ad8cdd1b80e2270b06d4494c38737224e7e81
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 01:38:59 +0200

    Added dbContext

---
+++ src/Application/Common/Interfaces/ITranslateDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ITranslateTrainer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Application.Common.Interfaces
+{
+    public interface ITranslateDbContext
+    {
+        public DbSet<Text> Texts { get; set; }
+        public DbSet<Translation> Translations { get; set; }
+
+        public Task SaveChangesAsync();
+    }
+}
---
+++ src/Infrastructure/Persistence/TranslationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using ITranslateTrainer.Domain.Entities;
+using ITranslateTrainer.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Infrastructure.Persistence
+{
+    public class TranslationDbContext : DbContext, ITranslateDbContext
+    {
+        public DbSet<Text> Texts { get; set; }
+        public DbSet<Translation> Translations { get; set; }
+
+        public Task SaveChangesAsync() => base.SaveChangesAsync();
+    }
+}
