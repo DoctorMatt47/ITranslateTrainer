727ed73dbba01e4b463ad6eb9437999981e97772
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 22:23:28 +0200

    Added mapping profiles

---
+++ src/Application/Common/Mappings/TextMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ITranslateTrainer.Application.Translations.Commands;
+using ITranslateTrainer.Application.Translations.Queries;
+using ITranslateTrainer.Domain.Entities;
+
+namespace ITranslateTrainer.Application.Common.Mappings
+{
+    public class TextMappingProfile : Profile
+    {
+        public TextMappingProfile()
+        {
+            CreateMap<CreateTextRequest, Text>();
+            CreateMap<Text, GetTextResponse>();
+        }
+    }
+}
---
+++ src/Application/Common/Mappings/TranslationMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ITranslateTrainer.Application.Translations.Queries;
+using ITranslateTrainer.Domain.Entities;
+
+namespace ITranslateTrainer.Application.Common.Mappings
+{
+    public class TranslationMappingProfile : Profile
+    {
+        public TranslationMappingProfile()
+        {
+            CreateMap<Translation, GetTranslationResponse>();
+        }
+    }
+}
