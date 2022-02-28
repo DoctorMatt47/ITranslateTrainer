4ab043ec23b1346c0441a15c62db159ea1f9f3fd
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 22:26:05 +0200

    Added get translations query

---
+++ src/Application/Translations/Queries/GetTranslationsQuery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using ITranslateTrainer.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Application.Translations.Queries
+{
+    public record GetTranslationsQuery : IRequest<IEnumerable<GetTranslationResponse>>;
+
+    public record GetTranslationsQueryHandler(ITranslateDbContext Context, IMapper Mapper) :
+        IRequestHandler<GetTranslationsQuery, IEnumerable<GetTranslationResponse>>
+    {
+        public async Task<IEnumerable<GetTranslationResponse>> Handle(GetTranslationsQuery _,
+            CancellationToken cancellationToken)
+        {
+            var translations = await Context.Translations.ToListAsync(cancellationToken);
+            var textIds = translations.Select(t => new List<int> {t.FirstId, t.SecondId}).SelectMany(t => t).Distinct();
+            await Context.Texts.Where(t => textIds.Contains(t.Id)).ToListAsync(cancellationToken);
+            return translations.Select(t => Mapper.Map<GetTranslationResponse>(t));
+        }
+    }
+}
