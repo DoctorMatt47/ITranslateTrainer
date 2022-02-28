118adeb81e5c5db5051a7a1ea0d40ff562b945b8
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 22:27:03 +0200

    Added service collection extension for application project

---
+++ src/Application/Common/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITranslateTrainer.Application.Common.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+       services.AddAutoMapper(Assembly.GetExecutingAssembly());
+       services.AddMediatR(Assembly.GetExecutingAssembly());
+       return services;
+    }
+}
