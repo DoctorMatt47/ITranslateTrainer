51ffd89b533e3b404b63b4706ba66c0560459172
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-01 00:19:03 +0200

    Renamed db context

---
+++ src/Infrastructure/Persistence/TranslateDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using ITranslateTrainer.Application.Common.Interfaces;
+using ITranslateTrainer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Infrastructure.Persistence;
+
+public class TranslateDbContext : DbContext, ITranslateDbContext
+{
+    public TranslateDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Text> Texts { get; set; } = null!;
+    public DbSet<Translation> Translations { get; set; } = null!;
+
+    public Task SaveChangesAsync() => base.SaveChangesAsync();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
