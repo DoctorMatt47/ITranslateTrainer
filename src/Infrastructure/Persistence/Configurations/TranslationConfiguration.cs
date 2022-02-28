0749a7f9d0b2f2d659fefe4bc13a2756a842fbd3
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-02-28 22:26:44 +0200

    Added translation configuration

---
+++ src/Infrastructure/Persistence/Configurations/TranslationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ITranslateTrainer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITranslateTrainer.Infrastructure.Persistence.Configurations
+{
+    public class TranslationConfiguration : IEntityTypeConfiguration<Translation>
+    {
+        public void Configure(EntityTypeBuilder<Translation> builder)
+        {
+            builder.HasOne(t => t.First);
+            builder.HasOne(t => t.Second);
+        }
+    }
+}
