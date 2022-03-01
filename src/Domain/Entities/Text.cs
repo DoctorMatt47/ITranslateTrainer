19d19c24a2d5aca190a54445b8c9a8bdbfee1792
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-01 21:17:05 +0200

    Created text configuration

--- src/Domain/Entities/Text.cs
+++ src/Domain/Entities/Text.cs
@@ -1,13 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using ITranslateTrainer.Domain.Common;
+﻿using ITranslateTrainer.Domain.Common;
 using ITranslateTrainer.Domain.Enums;
 
 namespace ITranslateTrainer.Domain.Entities;
 
 public class Text : IntIdEntity
 {
-    [MaxLength(50)] public string String { get; set; } = string.Empty;
+    public string String { get; set; } = string.Empty;
     public Language Language { get; set; }
-    public bool CanBeOption { get; set; }
-    public bool CanBeTested { get; set; }
+    public bool CanBeOption { get; set; } = true;
+    public bool CanBeTested { get; set; } = true;
 }
---
+++ src/Infrastructure/Persistence/Configurations/TextConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using ITranslateTrainer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITranslateTrainer.Infrastructure.Persistence.Configurations;
+
+public class TextConfiguration : IEntityTypeConfiguration<Text>
+{
+    public void Configure(EntityTypeBuilder<Text> builder)
+    {
+        builder.Property(t => t.String).HasMaxLength(50).IsRequired();
+    }
+}
