b1937545f96b420c607e66704579738cdb0d7545
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-01 21:16:28 +0200

    Added infrastructure service collection extension

---
+++ src/Infrastructure/Common/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using ITranslateTrainer.Domain.Interfaces;
+using ITranslateTrainer.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITranslateTrainer.Infrastructure.Common.Extensions;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        if (connectionString is not null)
+            services.AddDbContext<ITranslateDbContext, TranslateDbContext>(options => options.UseSqlite());
+        else
+            services.AddDbContext<ITranslateDbContext, TranslateDbContext>(options =>
+                options.UseInMemoryDatabase("ITranslateTrainerInMemoryDb"));
+
+        return services;
+    }
+}
