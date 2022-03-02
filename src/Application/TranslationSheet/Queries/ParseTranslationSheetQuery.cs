9be7bab867e0902bb871fdf8716a3d217d50d7d7
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:49:09 +0200

    Added parse translation sheet query

---
+++ src/Application/TranslationSheet/Queries/ParseTranslationSheetQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using MiniExcelLibs;
+
+namespace ITranslateTrainer.Application.TranslationSheet.Queries;
+
+public record ParseTranslationSheetQuery(Stream SheetStream) : IRequest<IEnumerable<ParseTranslationResponse>>;
+
+public record ParseTranslationSheetQueryHandler :
+    IRequestHandler<ParseTranslationSheetQuery, IEnumerable<ParseTranslationResponse>>
+{
+    public async Task<IEnumerable<ParseTranslationResponse>> Handle(ParseTranslationSheetQuery request,
+        CancellationToken cancellationToken)
+    {
+        var sheet = await request.SheetStream.QueryAsync();
+        return sheet.Select(row => new ParseTranslationResponse(row["A"].ToString(), row["B"].ToString(),
+            row["C"].ToString(), row["D"].ToString())).ToList();
+    }
+}
