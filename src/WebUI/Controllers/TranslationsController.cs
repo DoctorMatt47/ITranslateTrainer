e0242ffb3ed92ecde499192af3fa771785bf858a
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 23:38:45 +0200

    Implemented translations controller

--- src/WebUI/Controllers/TranslationsController.cs
+++ src/WebUI/Controllers/TranslationsController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITranslateTrainer.Application.Common.Responses;
+using ITranslateTrainer.Application.Translations.Commands;
+using ITranslateTrainer.Application.Translations.Queries;
+using ITranslateTrainer.Application.TranslationSheet.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITranslateTrainer.WebUI.Controllers;
 
+[ApiController]
 [Route("api/[controller]")]
 public class TranslationsController : Controller
 {
+    private readonly IMediator _mediator;
+
+    public TranslationsController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GetTranslationResponse>>> GetTranslations() =>
+        Ok(await _mediator.Send(new GetTranslationsQuery()));
+
+    [HttpPost]
+    public async Task<ActionResult<IntIdResponse>> CreateTranslation(CreateTranslationCommand command) =>
+        Ok(await _mediator.Send(command));
+
     [HttpPost("sheet")]
-    public async Task CreateTranslationSheet([FromBody] IFormFile sheet)
-    {
-    }
+    public async Task<ActionResult<IEnumerable<object>>> CreateTranslationSheet([FromBody] IFormFile sheet) =>
+        Ok(await _mediator.Send(new CreateTranslationSheetCommand(sheet.OpenReadStream())));
 }
