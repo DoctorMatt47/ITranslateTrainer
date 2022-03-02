16f4a4f7b9c7a32e75ab2a607e1e948985fc113f
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:48:00 +0200

    Added create translation sheet command

---
+++ src/Application/TranslationSheet/Commands/CreateTranslationSheetCommand.cs
@@ -0,0 +1,53 @@
+﻿using ITranslateTrainer.Application.Common.Exceptions;
+using ITranslateTrainer.Application.Languages.Queries;
+using ITranslateTrainer.Application.Texts.Commands;
+using ITranslateTrainer.Application.Texts.Queries;
+using ITranslateTrainer.Application.Translations.Commands;
+using ITranslateTrainer.Application.TranslationSheet.Queries;
+using MediatR;
+
+namespace ITranslateTrainer.Application.TranslationSheet.Commands;
+
+public record CreateTranslationSheetCommand(Stream SheetStream) : IRequest<IEnumerable<object>>;
+
+public record CreateTranslationSheetCommandHandler(IMediator Mediator) :
+    IRequestHandler<CreateTranslationSheetCommand, IEnumerable<object>>
+{
+    public async Task<IEnumerable<object>> Handle(CreateTranslationSheetCommand request,
+        CancellationToken cancellationToken)
+    {
+        var parsedTranslations =
+            await Mediator.Send(new ParseTranslationSheetQuery(request.SheetStream), cancellationToken);
+
+        var response = new List<object>();
+        foreach (var parsedTranslation in parsedTranslations)
+        {
+            var (firstLanguage, secondLanguage, firstText, secondText) = parsedTranslation;
+            try
+            {
+                var firstTextFiltered = await Mediator.Send(new FilterTextQuery(firstText), cancellationToken);
+                var secondTextFiltered = await Mediator.Send(new FilterTextQuery(secondText), cancellationToken);
+
+                var firstLanguageFiltered =
+                    await Mediator.Send(new ParseLanguageQuery(firstLanguage), cancellationToken);
+                var secondLanguageFiltered =
+                    await Mediator.Send(new ParseLanguageQuery(secondLanguage), cancellationToken);
+
+                var translationId = await Mediator.Send(new CreateTranslationCommand(
+                    new CreateTextCommand(firstTextFiltered, firstLanguageFiltered),
+                    new CreateTextCommand(secondTextFiltered, secondLanguageFiltered)), cancellationToken);
+
+                response.Add(translationId);
+            }
+            catch (BadRequestException e)
+            {
+                response.Add(new
+                {
+                    errorMessage = e.Message
+                });
+            }
+        }
+
+        return response;
+    }
+}
