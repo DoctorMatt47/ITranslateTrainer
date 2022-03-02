4cd68cd0a1ae04d9b643ce35f58f497f9749f8c7
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:47:21 +0200

    Renamed and moved create text request

---
+++ src/Application/Texts/Commands/CreateTextCommand.cs
@@ -0,0 +1,7 @@
+﻿using ITranslateTrainer.Application.Common.Responses;
+using ITranslateTrainer.Domain.Enums;
+using MediatR;
+
+namespace ITranslateTrainer.Application.Texts.Commands;
+
+public record CreateTextCommand(string String, Language Language) : IRequest<IntIdResponse>;
--- src/Application/Translations/Commands/CreateTextRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using ITranslateTrainer.Domain.Enums;
-
-namespace ITranslateTrainer.Application.Translations.Commands;
-
-public record CreateTextRequest(string String, Language Language);
