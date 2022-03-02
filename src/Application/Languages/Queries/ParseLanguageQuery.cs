7a7e44f56ef2f7a6def0bbc86a09f01e736b75d2
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:48:32 +0200

    Added parse language query

---
+++ src/Application/Languages/Queries/ParseLanguageQuery.cs
@@ -0,0 +1,17 @@
+﻿using ITranslateTrainer.Application.Common.Exceptions;
+using ITranslateTrainer.Domain.Enums;
+using MediatR;
+
+namespace ITranslateTrainer.Application.Languages.Queries;
+
+public record ParseLanguageQuery(string? LanguageString) : IRequest<Language>;
+
+public record ParseLanguageQueryHandler : IRequestHandler<ParseLanguageQuery, Language>
+{
+    public Task<Language> Handle(ParseLanguageQuery request, CancellationToken cancellationToken)
+    {
+        if (request.LanguageString is null) throw new BadRequestException("Language string is null");
+        if (Enum.TryParse(request.LanguageString, out Language language)) return Task.FromResult(language);
+        throw new BadRequestException("Language string cannot be parsed");
+    }
+}
