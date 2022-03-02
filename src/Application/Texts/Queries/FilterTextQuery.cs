f73d5653e6c42fe57f59556a929fe54a9aec00f0
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:48:19 +0200

    Added filter text query

---
+++ src/Application/Texts/Queries/FilterTextQuery.cs
@@ -0,0 +1,18 @@
+﻿using ITranslateTrainer.Application.Common.Exceptions;
+using MediatR;
+
+namespace ITranslateTrainer.Application.Texts.Queries;
+
+public record FilterTextQuery(string? TextString) : IRequest<string>;
+
+public record FilterTextQueryHandler : IRequestHandler<FilterTextQuery, string>
+{
+    public Task<string> Handle(FilterTextQuery request, CancellationToken _)
+    {
+        if (request.TextString is null) throw new BadRequestException("Text string is null");
+        return Task.FromResult(request.TextString
+            .Replace("\n", "")
+            .Replace("\t", "")
+            .Trim());
+    }
+}
