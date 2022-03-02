8153790bab9982927cfd25496716a4f679a42b33
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-02 22:50:37 +0200

    Added linq extension

---
+++ src/Application/Common/Extensions/LinqExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace ITranslateTrainer.Application.Common.Extensions;
+
+public static class LinqExtension
+{
+    public static async Task<IEnumerable<TResponse>> WhenAllAsync<TResponse>(this IEnumerable<Task<TResponse>> tasks)
+    {
+        var responses = new List<TResponse>();
+        foreach (var task in tasks)
+        {
+            var response = await task;
+            responses.Add(response);
+        }
+
+        return responses;
+    }
+}
