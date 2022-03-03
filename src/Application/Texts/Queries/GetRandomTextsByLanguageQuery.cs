a70dcd875025f3a14ae91d2bcf3f720eadc9b111
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-03 17:42:26 +0200

    Added get random texts by language query

---
+++ src/Application/Texts/Queries/GetRandomTextsByLanguageQuery.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ITranslateTrainer.Application.Common.Extensions;
+using ITranslateTrainer.Domain.Enums;
+using ITranslateTrainer.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Application.Texts.Queries;
+
+public record GetRandomTextsByLanguageQuery(Language Language, int Count) : IRequest<IEnumerable<GetTextResponse>>;
+
+public record GetRandomTextsByLanguageQueryHandler(ITranslateDbContext Context, IMapper Mapper) :
+    IRequestHandler<GetRandomTextsByLanguageQuery, IEnumerable<GetTextResponse>>
+{
+    public async Task<IEnumerable<GetTextResponse>> Handle(GetRandomTextsByLanguageQuery request,
+        CancellationToken cancellationToken)
+    {
+        var (language, count) = request;
+        var randomTexts = await Context.Texts
+            .Where(t => t.Language == language && t.CanBeTested)
+            .Shuffle().Take(count).ToListAsync(cancellationToken);
+
+        return randomTexts.Select(t => Mapper.Map<GetTextResponse>(t));
+    }
+}
