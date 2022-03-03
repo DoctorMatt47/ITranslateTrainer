6d862d645ee093c6abd36863d7ff241ad873a13c
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-03 23:45:16 +0200

    Implemented quiz controller

--- src/WebUI/Controllers/QuizController.cs
+++ src/WebUI/Controllers/QuizController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ITranslateTrainer.Application.Quiz.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ITranslateTrainer.WebUI.Controllers;
 
@@ -6,4 +8,11 @@ namespace ITranslateTrainer.WebUI.Controllers;
 [Route("api/[controller]")]
 public class QuizController : Controller
 {
+    private readonly IMediator _mediator;
+
+    public QuizController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GetQuizResponse>>> Get([FromQuery] GetQuizQuery query) =>
+        Ok(await _mediator.Send(query));
 }
