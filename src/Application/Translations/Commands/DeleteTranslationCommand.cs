c1e18c27d685f458966bdc2421ad5b3829306296
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-03-04 16:35:37 +0200

    Added delete translation command

---
+++ src/Application/Translations/Commands/DeleteTranslationCommand.cs
@@ -0,0 +1,35 @@
+﻿using ITranslateTrainer.Application.Common.Exceptions;
+using ITranslateTrainer.Application.Translations.Queries;
+using ITranslateTrainer.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITranslateTrainer.Application.Translations.Commands;
+
+public record DeleteTranslationCommand(int Id) : IRequest;
+
+public record DeleteTranslationCommandHandler
+    (ITranslateDbContext Context, IMediator Mediator) : IRequestHandler<DeleteTranslationCommand>
+{
+    public async Task<Unit> Handle(DeleteTranslationCommand request, CancellationToken cancellationToken)
+    {
+        var translationToDelete = await Context.Translations.Include(t => t.First).Include(t => t.Second)
+            .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
+
+        if (translationToDelete is null)
+            throw new BadRequestException($"There is no translation with id = {request.Id}");
+
+        Context.Translations.Remove(translationToDelete);
+
+        var firstTextTranslations = await Mediator.Send(new GetTranslationsByTextIdQuery(translationToDelete.FirstId),
+            cancellationToken);
+        if (firstTextTranslations.Count() <= 1) Context.Texts.Remove(translationToDelete.First);
+
+        var secondTextTranslations = await Mediator.Send(new GetTranslationsByTextIdQuery(translationToDelete.SecondId),
+            cancellationToken);
+        if (secondTextTranslations.Count() <= 1) Context.Texts.Remove(translationToDelete.Second);
+
+        await Context.SaveChangesAsync();
+        return Unit.Value;
+    }
+}
---
+++ tests/Application.UnitTests/Translations/Commands/DeleteTranslationTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using ITranslateTrainer.Application.Translations.Commands;
+using ITranslateTrainer.Domain.Entities;
+using ITranslateTrainer.Domain.Enums;
+using ITranslateTrainer.Domain.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ITranslateTrainer.Application.UnitTests.Translations.Commands;
+
+public class DeleteTranslationTests
+{
+    private readonly ITranslateDbContext _context;
+    private readonly IMediator _mediator;
+
+    public DeleteTranslationTests(IMediator mediator, ITranslateDbContext context)
+    {
+        _mediator = mediator;
+        _context = context;
+    }
+
+    [Fact]
+    public async Task ShouldDeleteTranslation()
+    {
+        var translationToAdd = new Translation
+        {
+            First = new Text
+            {
+                Language = Language.English,
+                String = "delete translation"
+            },
+            Second = new Text
+            {
+                Language = Language.Russian,
+                String = "удаление перевода"
+            }
+        };
+        _context.Translations.Add(translationToAdd);
+        await _context.SaveChangesAsync();
+
+        await _mediator.Send(new DeleteTranslationCommand(translationToAdd.Id));
+        var translation = await _context.Translations.FirstOrDefaultAsync(t => t.Id == translationToAdd.Id);
+        Assert.Null(translation);
+
+        var first = await _context.Texts.FirstOrDefaultAsync(t => t.Id == translationToAdd.FirstId);
+        Assert.Null(first);
+
+        var second = await _context.Texts.FirstOrDefaultAsync(t => t.Id == translationToAdd.SecondId);
+        Assert.Null(second);
+    }
+}
