b2489c37a3475e04b072e37b47f3f376998c5496
DoctorMatt47 <DoctorMatt47@gmail.com>
2022-04-21 23:28:07 +0300

    Prettified error controller

--- src/WebUI/Controllers/ErrorController.cs
+++ src/WebUI/Controllers/ErrorController.cs
@@ -13,12 +13,14 @@ public class ErrorController : Controller
     public ActionResult<object> HandleError()
     {
         var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
         var responseCode = exception switch
         {
             BadRequestException or DomainArgumentException => 400,
             NotFoundException => 404,
             _ => 500
         };
+
         return StatusCode(responseCode, new ErrorResponse(exception?.Message, exception?.StackTrace));
     }
 }
