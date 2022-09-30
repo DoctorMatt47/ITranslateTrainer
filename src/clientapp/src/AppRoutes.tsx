ebfd642711a6c76103aea4cfcfcc8a629ce20c10
m.holtsev <DoctorMatt47@gmail.com>
2022-09-30 19:02:04 +0300

    app routes

---
+++ src/clientapp/src/AppRoutes.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import AppShell from "./common/components/AppShell/AppShell";
+import Menu from "./Menu/Menu";
+import NotFound from "./NotFound/NotFound";
+import Translations from "./Translations/Translations";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <AppShell>
+        <Routes>
+          <Route path="/" element={<Navigate to="/menu" replace/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/translations" element={<Translations/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </AppShell>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
