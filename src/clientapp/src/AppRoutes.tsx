d2fbd7f2bb366a6c48c6b48799b5dba9d1de89b7
m.holtsev <DoctorMatt47@gmail.com>
2022-10-01 17:19:53 +0300

    Added quiz route

--- src/clientapp/src/AppRoutes.tsx
+++ src/clientapp/src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import AppShell from "./common/components/AppShell/AppShell";
 import Menu from "./Menu/Menu";
 import NotFound from "./NotFound/NotFound";
 import Translations from "./Translations/Translations";
+import Quiz from "./Quiz/Quiz";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
           <Route path="/" element={<Navigate to="/menu" replace/>}/>
           <Route path="/menu" element={<Menu/>}/>
           <Route path="/translations" element={<Translations/>}/>
+          <Route path="/quiz" element={<Quiz/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </AppShell>
