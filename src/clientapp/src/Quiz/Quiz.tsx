b59c476575d09697bd7c67ada00fcf45870ca09b
m.holtsev <DoctorMatt47@gmail.com>
2022-10-05 15:08:34 +0300

    Implemented quiz page logic

--- src/clientapp/src/Quiz/Quiz.tsx
+++ src/clientapp/src/Quiz/Quiz.tsx
@@ -1,12 +1,26 @@
-import "./Quiz.scss";
 import QuizSettings from "./Settings/QuizSettings";
+import {QuizTestData} from "../common/services/quiz-service";
+import {useState} from "react";
+import QuizTests from "./Tests/QuizTests";
+
+interface QuizState {
+  tests: QuizTestData[];
+  isFinished: boolean;
+}
 
 const Quiz = () => {
-  return (
-    <>
-      <QuizSettings/>
-    </>
-  );
+  const [state, setState] = useState<QuizState>({tests: [], isFinished: false});
+
+  if (state.tests.length === 0)
+    return <QuizSettings
+      setTests={tests => setState({...state, tests: tests})}/>;
+
+  if (!state.isFinished)
+    return <QuizTests tests={state.tests}
+                      restart={() => setState({...state, tests: []})}
+                      finish={() => setState({...state, isFinished: true})}/>;
+
+  return <></>;
 }
 
 export default Quiz;
