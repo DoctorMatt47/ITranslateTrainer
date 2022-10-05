d78077392525af73aef03da6930b912352a22c42
m.holtsev <DoctorMatt47@gmail.com>
2022-10-05 15:07:24 +0300

    Added quiz settings component

--- src/clientapp/src/Quiz/Settings/QuizSettings.tsx
+++ src/clientapp/src/Quiz/Settings/QuizSettings.tsx
@@ -1,8 +1,34 @@
 import Display from "../../common/components/Display/Display";
 import {Col, Container, Row} from "react-bootstrap";
 import AppButton from "../../common/components/AppButton/AppButton";
+import "./QuizSettings.scss";
+import React, {SyntheticEvent, useState} from "react";
+import {getQuiz, QuizTestData} from "../../common/services/quiz-service";
 
-const QuizSettings = () => {
+interface LanguageOption {
+  to: string;
+  from: string;
+}
+
+interface QuizSettingsState {
+  testCount: number;
+  optionCount: number;
+  language: LanguageOption;
+}
+
+interface QuizSettingsProps {
+  setTests: (tests: QuizTestData[]) => void;
+}
+
+const QuizSettings = ({setTests}: QuizSettingsProps) => {
+  const languagePairs = [{first: "English", second: "Russian"}];
+
+  const [state, setState] = useState<QuizSettingsState>({
+    testCount: 0,
+    optionCount: 0,
+    language: {from: languagePairs[0].first, to: languagePairs[0].second},
+  });
+
   return (
     <>
       <Display size={4}
@@ -21,7 +47,8 @@ const QuizSettings = () => {
             <Col lg={4} sm={12} className="text-center">
               <input className="setting-input m-3 w-75"
                      id="test-count"
-                     type="number"/>
+                     type="number"
+                     onChange={setTestCount(state, setState)}/>
             </Col>
           </Row>
           <Row className="align-items-center">
@@ -32,8 +59,9 @@ const QuizSettings = () => {
               </label>
             </Col>
             <Col lg={4} className="text-center">
-              <input className="setting-input m-3 w-75" id="option-count"
-                     type="number"/>
+              <input className="setting-input m-3 w-75"
+                     type="number"
+                     onChange={setOptionCount(state, setState)}/>
             </Col>
           </Row>
           <Row className="mb-4 align-items-center">
@@ -44,16 +72,20 @@ const QuizSettings = () => {
               </label>
             </Col>
             <Col lg={4} className="text-center">
-              <select className="setting-input m-3 w-75" id="language-select">
-                <option value="12">English-Russian</option>
-                <option value="21">Russian-English</option>
+              <select className="setting-input m-3 w-75"
+                      onChange={setLanguage(state, setState)}>
+                {languagePairs.map(l =>
+                  <>
+                    <option>{[l.first, l.second].join("-")}</option>
+                    <option>{[l.second, l.first].join("-")}</option>
+                  </>)}
               </select>
             </Col>
           </Row>
           <Row className="justify-content-center">
             <AppButton className="w-75">
               Start
-              <button id="start-button"></button>
+              <button onClick={start(state, setTests)}/>
             </AppButton>
           </Row>
         </Container>
@@ -61,5 +93,56 @@ const QuizSettings = () => {
     </>
   );
 }
+
+const setTestCount = (
+  state: QuizSettingsState,
+  setState: (x: QuizSettingsState) => void
+) => {
+  return (e: SyntheticEvent<HTMLInputElement>) => {
+    const newState = {
+      ...state,
+      testCount: parseInt(e.currentTarget.value),
+    };
+    setState(newState);
+  }
+}
+
+const setOptionCount = (
+  state: QuizSettingsState,
+  setState: (x: QuizSettingsState) => void
+) => {
+  return (e: SyntheticEvent<HTMLInputElement>) => {
+    const newState = {
+      ...state,
+      optionCount: parseInt(e.currentTarget.value),
+    };
+    setState(newState);
+  }
+}
+
+const setLanguage = (
+  state: QuizSettingsState,
+  setState: (x: QuizSettingsState) => void
+) => {
+  return (e: SyntheticEvent<HTMLSelectElement>) => {
+    const languages = e.currentTarget.value.split("-");
+    const newState = {
+      ...state,
+      language: {from: languages[0], to: languages[1]}
+    };
+    setState(newState);
+  };
+};
+
+const start = (
+  state: QuizSettingsState,
+  setTests: (tests: QuizTestData[]) => void
+) => {
+  return async () => {
+    const tests = await getQuiz({...state.language, ...state});
+    if (tests.length === 0) return;
+    setTests(tests);
+  };
+};
 
 export default QuizSettings;
