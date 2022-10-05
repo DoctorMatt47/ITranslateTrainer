ba1463d44c22c61feaddc67c2e284f6bd1ef5e06
m.holtsev <DoctorMatt47@gmail.com>
2022-10-05 19:21:41 +0300

    Added set result prop to quiz test component

--- src/clientapp/src/Quiz/Tests/QuizTests.tsx
+++ src/clientapp/src/Quiz/Tests/QuizTests.tsx
@@ -3,51 +3,81 @@ import "./QuizTests.scss";
 import AppButton from "../../common/components/AppButton/AppButton";
 import {QuizTestData} from "../../common/services/quiz-service";
 import {useState} from "react";
-import QuizTest from "./QuizTest/QuizTest";
+import QuizTest, {Result} from "./QuizTest/QuizTest";
+import QuizResult, {QuizResultCount} from "./Result/QuizResult";
+
+interface QuizTestsState {
+  index: number;
+  count: QuizResultCount;
+}
 
 interface QuizTestsProps {
   tests: QuizTestData[];
   restart: () => void;
-  finish: () => void;
-}
-
-interface QuizTestsState {
-  index: number;
 }
 
 const QuizTests = (props: QuizTestsProps) => {
-  const [state, setState] = useState<QuizTestsState>({index: 0});
+  const [state, setState] = useState<QuizTestsState>({
+    index: 0,
+    count: {correct: 0, incorrect: 0, skipped: props.tests.length}
+  });
 
-  return (
-    <Container className="test">
-      <Row>
-        <Col lg={3}/>
+  if (state.index !== props.tests.length)
+    return (
+      <Container className="test">
+        <Row>
+          <Col lg={3}/>
 
-        <Col lg sm className="text-center">
+          <Col lg sm className="text-center">
 
-          <QuizTest test={props.tests[state.index]} key={state.index}/>
+            <QuizTest test={props.tests[state.index]}
+                      key={state.index}
+                      setResult={setResult(state, setState)}/>
 
-          <Container className="p-0">
-            <Row>
-              <Col>
-                <AppButton className="next-button" label="Restart">
-                  <button onClick={props.restart}/>
-                </AppButton>
-              </Col>
+            <Container className="p-0">
+              <Row>
+                <Col>
+                  <AppButton className="next-button" label="Restart">
+                    <button onClick={props.restart}/>
+                  </AppButton>
+                </Col>
 
-              <Col className="text-end">
-                <AppButton className="next-button" label="Next">
-                  <button onClick={nextTest(props, state, setState)}/>
-                </AppButton>
-              </Col>
-            </Row>
-          </Container>
-        </Col>
+                <Col className="text-end">
+                  <AppButton className="next-button" label="Next">
+                    <button onClick={nextTest(props, state, setState)}/>
+                  </AppButton>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
 
-        <Col lg={3}/>
-      </Row>
-    </Container>
-  );
+          <Col lg={3}/>
+        </Row>
+      </Container>
+    );
+
+  return <QuizResult count={state.count}/>
+}
+
+const setResult = (
+  state: QuizTestsState,
+  setState: (state: QuizTestsState) => void
+) => {
+  return (result: Result) => {
+    if (result === Result.Skipped) return;
+
+    const newCount = {
+      correct: state.count.correct,
+      incorrect: state.count.incorrect,
+      skipped: state.count.skipped
+    }
+
+    if (result === Result.Correct) newCount.correct++;
+    if (result === Result.Incorrect) newCount.incorrect++;
+
+    newCount.skipped--;
+    setState({...state, count: newCount});
+  }
 }
 
 const nextTest = (
@@ -57,10 +87,9 @@ const nextTest = (
 ) => {
   return () => {
     if (state.index !== props.tests.length) {
-      setState({index: state.index + 1})
+      setState({...state, index: state.index + 1})
       return;
     }
-    props.finish();
   }
 }
 
