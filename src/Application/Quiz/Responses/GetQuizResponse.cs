78a7b739a95c08a05a605a6d5ab13d2af13c2ca4
m.holtsev <DoctorMatt47@gmail.com>
2022-11-20 09:47:48 +0200

    Removed quiz

--- src/Application/Quiz/Responses/GetQuizResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿using ITranslateTrainer.Application.Common.Responses;
-
-namespace ITranslateTrainer.Application.Quiz.Responses;
-
-public record GetQuizResponse(
-    string String,
-    IEnumerable<OptionResponse> Options);
