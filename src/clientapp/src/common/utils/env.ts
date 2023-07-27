eda70b18fe07db9ade184ea82108edccd64426c0
m.holtsev <DoctorMatt47@gmail.com>
2023-07-27 21:26:19 +0300

    Extended env.ts file

--- src/clientapp/src/common/utils/env.ts
+++ src/clientapp/src/common/utils/env.ts
@@ -1,9 +1,3 @@
 export var baseUrl: string;
 
-switch (process.env.NODE_ENV) {
-  case "production":
-    baseUrl = "http://192.168.0.102:5207";
-    break;
-  default:
-    baseUrl = "http://localhost:5207";
-}
+baseUrl = "http://localhost:5207";
