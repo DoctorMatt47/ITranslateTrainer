acb6a548d42e7c93d71fdac5030471bfeaa001aa
m.holtsev <DoctorMatt47@gmail.com>
2023-08-08 03:35:24 +0300

    Removed page.ts file

--- src/svelteapp/src/routes/+page.svelte
+++ src/svelteapp/src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+
+  onMount(async () => {
+    await goto("/menu");
+  });
+
+</script>
+
 <slot />
--- src/svelteapp/src/routes/+page.ts
+++
@@ -1,6 +0,0 @@
-﻿import { redirect } from "@sveltejs/kit";
-import type { PageLoad } from "./$types";
-
-export const load = (() => {
-  throw redirect(302, "/menu");
-}) satisfies PageLoad;
