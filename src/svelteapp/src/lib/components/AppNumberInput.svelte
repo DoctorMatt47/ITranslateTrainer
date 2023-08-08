8630f247dde784c4b12d8a15b99b38f7219362af
m.holtsev <DoctorMatt47@gmail.com>
2023-08-08 03:36:16 +0300

    Implemented quiz settings

---
+++ src/svelteapp/src/lib/components/AppNumberInput.svelte
@@ -0,0 +1,24 @@
+﻿<script lang="ts">
+  export let value: number;
+  export let className = "";
+
+</script>
+
+<input {...$$restProps}
+       bind:value
+       class="input text-center p-1 dark:bg-black/0 placeholder:text-surface-600 {className}"
+       type="number"
+/>
+
+<style>
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        /* display: none; <- Crashes Chrome on hover */
+        -webkit-appearance: none;
+        margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+    }
+
+    input[type=number] {
+        -moz-appearance: textfield; /* Firefox */
+    }
+</style>
---
+++ src/svelteapp/src/routes/quiz/settings/+page.svelte
@@ -0,0 +1,43 @@
+﻿<script lang="ts">
+  import AppHeading from "$lib/components/AppHeading.svelte";
+  import AppNumberInput from "$lib/components/AppNumberInput.svelte";
+  import AppTextInput from "$lib/components/AppTextInput.svelte";
+  import AppVariant from "$lib/components/AppVariant.svelte";
+  import type { PutTestRequest } from "$lib/services/test-service";
+  import { putTest } from "$lib/services/test-service";
+  import { testSettingsStore, testStore } from "$lib/stores";
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+
+  const settings: PutTestRequest = $testSettingsStore;
+
+  onMount(() => {
+    console.log(settings);
+  });
+
+  async function start() {
+    testSettingsStore.set(settings);
+    const test = await putTest(settings);
+    testStore.set(test);
+    await goto("/test");
+  }
+
+</script>
+
+<AppHeading>Settings</AppHeading>
+
+<form on:submit={start}>
+  <div class="w-1/2 mx-auto">
+    <div class="grid grid-rows-4 grid-cols-2 gap-6 text-center items-center">
+      <div>From:</div>
+      <AppTextInput bind:value={settings.from} placeholder="from" />
+      <div>To:</div>
+      <AppTextInput bind:value={settings.to} placeholder="to" />
+      <div>Option count</div>
+      <AppNumberInput bind:value={settings.optionCount} />
+      <button class="w-full col-span-2" type="submit">
+        <AppVariant>Start</AppVariant>
+      </button>
+    </div>
+  </div>
+</form>
