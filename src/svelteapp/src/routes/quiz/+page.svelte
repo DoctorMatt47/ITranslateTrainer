6b1d24a280058c6e891ccf87b6d67ce9ef79da7d
m.holtsev <DoctorMatt47@gmail.com>
2023-08-08 03:36:57 +0300

    First part quiz component

---
+++ src/svelteapp/src/routes/quiz/+page.svelte
@@ -0,0 +1,23 @@
+﻿<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+  import { testStore } from "$lib/stores";
+  import AppHeading from "$lib/components/AppHeading.svelte";
+  import AppVariant from "$lib/components/AppVariant.svelte";
+
+  let test = $testStore;
+
+  onMount(async () => {
+    if (test) {
+      await goto("/quiz/settings");
+    }
+  });
+
+</script>
+
+<AppHeading>{test?.string}</AppHeading>
+
+<div class="flex flex-col">
+  <AppVariant>
+  </AppVariant>
+</div>
