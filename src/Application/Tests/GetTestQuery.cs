0464f439eb9126e39a74935b12fd43c77118656b
m.holtsev <DoctorMatt47@gmail.com>
2023-08-26 16:04:10 +0300

    Used null coalescing operator

--- src/Application/Tests/GetTestQuery.cs
+++ src/Application/Tests/GetTestQuery.cs
@@ -17,15 +17,11 @@ internal record GetTestQueryHandler : IRequestHandler<GetTestQuery, TestResponse
     public async Task<TestResponse> Handle(GetTestQuery request, CancellationToken cancellationToken)
     {
         var test = await _context.Set<Test>()
-            .Include(t => t.Text)
-            .Include(t => t.Options)
-            .ThenInclude(t => t.Text)
-            .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
-
-        if (test is null)
-        {
-            throw new NotFoundException($"There is no test with id: {request.Id}");
-        }
+                .Include(t => t.Text)
+                .Include(t => t.Options)
+                .ThenInclude(t => t.Text)
+                .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken)
+            ?? throw new NotFoundException($"There is no test with id: {request.Id}");
 
         if (!test.IsAnswered) throw new BadRequestException("You don't have permission to get not answered test");
 
