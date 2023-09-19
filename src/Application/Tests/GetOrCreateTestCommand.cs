62170e2aaf6e2c3aa390b951ea07dcab908a3c0f
m.holtsev <DoctorMatt47@gmail.com>
2023-09-20 00:29:58 +0300

    Refactored get or create test command

--- src/Application/Tests/GetOrCreateTestCommand.cs
+++ src/Application/Tests/GetOrCreateTestCommand.cs
@@ -1,6 +1,5 @@
 ﻿using ITranslateTrainer.Application.Common.Extensions;
 using ITranslateTrainer.Application.Common.Interfaces;
-using ITranslateTrainer.Application.TranslationTexts;
 using ITranslateTrainer.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,8 +7,8 @@ using Microsoft.EntityFrameworkCore;
 namespace ITranslateTrainer.Application.Tests;
 
 public record GetOrCreateTestCommand(
-        string From,
-        string To,
+        string FromLanguage,
+        string ToLanguage,
         int OptionCount)
     : IRequest<TestResponse>;
 
@@ -17,70 +16,73 @@ internal class CreateTestCommandHandler : IRequestHandler<GetOrCreateTestCommand
 {
     private readonly ITranslateDbContext _context;
 
-    public CreateTestCommandHandler(ITranslateDbContext context)
-    {
-        _context = context;
-    }
+    private GetOrCreateTestCommand _request = null!;
+
+    public CreateTestCommandHandler(ITranslateDbContext context) => _context = context;
 
     public async Task<TestResponse> Handle(
         GetOrCreateTestCommand request,
         CancellationToken cancellationToken)
     {
-        var (from, to, optionCount) = request;
+        _request = request;
 
         var test = await _context.Set<Test>()
+            .Where(t => t.OptionCount == _request.OptionCount)
             .Where(Is.Not(Test.IsAnsweredExpression))
-            .FirstOrDefaultAsync(t => t.OptionCount == optionCount, cancellationToken);
+            .FirstOrDefaultAsync(cancellationToken);
 
-        if (test is not null)
-        {
-            return test.ToResponse();
-        }
+        if (test is not null) return test.ToResponse();
 
-        test = await CreateTest(from, to, optionCount, cancellationToken);
+        test = await CreateRandomTest(cancellationToken);
+
         await _context.Set<Test>().AddAsync(test, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
 
         return test.ToResponse();
     }
 
-    private async Task<Test> CreateTest(
-        string from,
-        string to,
-        int optionCount,
-        CancellationToken cancellationToken)
+    private async Task<Test> CreateRandomTest(CancellationToken cancellationToken)
     {
         var text = await _context.Set<Text>()
-            .Where(t => t.Language == from)
+            .Where(t => t.Language == _request.FromLanguage)
             .Shuffle()
             .FirstAsync(cancellationToken);
 
-        var correct = text.GetTranslationTexts()
-            .Select(static text => new Option
-            {
-                Text = text,
-                IsCorrect = true,
-            })
-            .ToList();
-
-        var incorrect = await _context.Set<Text>()
-            .GetRandomCanBeOption(to, optionCount - correct.Count)
-            .Select(static text => new Option
-            {
-                Text = text,
-                IsCorrect = false,
-            })
-            .ToListAsync(cancellationToken);
-
-        var options = correct
-            .Concat(incorrect)
-            .Shuffle()
-            .ToList();
+        var options = await CreateRandomOptions(text, cancellationToken);
 
         return new Test
         {
             Text = text,
             Options = options,
         };
+    }
+
+    private async Task<List<Option>> CreateRandomOptions(Text text, CancellationToken cancellationToken)
+    {
+        var correctOptions = text.GetTranslationTexts()
+            .Select(t => new Option
+            {
+                Text = t,
+                IsCorrect = true,
+            })
+            .ToList();
+
+        var incorrectOptionCount = _request.OptionCount - correctOptions.Count;
+
+        var incorrectOptions = await _context.Set<Text>()
+            .Where(t => t.Language == _request.ToLanguage.ToLowerInvariant())
+            .Shuffle()
+            .Take(incorrectOptionCount)
+            .Select(t => new Option
+            {
+                Text = t,
+                IsCorrect = false,
+            })
+            .ToListAsync(cancellationToken);
+
+        return correctOptions
+            .Concat(incorrectOptions)
+            .Shuffle()
+            .ToList();
     }
 }
