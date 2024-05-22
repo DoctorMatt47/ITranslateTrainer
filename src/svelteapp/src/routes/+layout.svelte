e8dcaa51a4764c3f5ae1e7774c6b78e3cae51b47
m.holtsev <DoctorMatt47@gmail.com>
2024-05-22 23:56:31 +0300

    refactor

--- src/svelteapp/src/routes/+layout.svelte
+++ src/svelteapp/src/routes/+layout.svelte
@@ -1,9 +1,4 @@
-<script lang="ts">
-  // The ordering of these imports is critical to your app working properly
-  import "@skeletonlabs/skeleton/themes/theme-gold-nouveau.css";
-  // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-  import "@skeletonlabs/skeleton/styles/skeleton.css";
-  // Most of your app wide CSS should be put in this file
+<script>
   import "../app.postcss";
   import { AppBar, AppShell } from "@skeletonlabs/skeleton";
   import AppCard from "$lib/common/components/AppCard.svelte";
@@ -28,3 +23,5 @@
     </AppCard>
   </div>
 </AppShell>
+
+<style></style>
--- src/svelteapp/src/routes/+page.svelte
+++ src/svelteapp/src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { goto } from "$app/navigation";
 
   const { children } = $props();
 
-  onMount(async () => {
-    await goto("/menu");
+  $effect.pre(() => {
+    goto("/menu");
   });
 </script>
 
